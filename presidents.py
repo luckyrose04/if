e0fff8cc074fc72703462897bfbb010cf425dcb5
Clarisa Carruthers <clarisita.92@gmail.com>
2016-03-20 13:38:40 -0400

    version 3 of presidents- single quotes, enter, remove r, add space between operators and operands.py

--- presidents.py
+++ presidents.py
@@ -1,10 +1,10 @@
-in_file=open('presidents.txt', 'r')
-out_file=open('output.txt', 'w')
+in_file = open('presidents.txt')
+out_file = open('output.txt', 'w')
 
 president=[]
 
 for line in in_file:
-    wordlist= line.split(",")
+    wordlist = line.split(',')
     president.append(wordlist[0])
     president.append(wordlist[2])
 
