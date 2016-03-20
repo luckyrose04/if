03fa38e24764196faf84fbe613c0f6337a0a0823
Clarisa Carruthers <clarisita.92@gmail.com>
2016-03-20 18:03:27 -0400

    deleted lines 3-12,moved print, changed indentations

--- presidents.py
+++ presidents.py
@@ -1,21 +1,11 @@
 import csv
 
-in_file = open('presidents.txt')
-out_file = open('output.txt', 'w')
-
-president = []
-
-for line in in_file:
-    wordlist = line.split(',')
-    president.append(wordlist[0])
-    president.append(wordlist[2])
-
-print president
-
 president = []
 
 with open('presidents.txt') as csvfile:
-	reader = csv.reader(csvfile)
-	for row in reader:
-		president.append(row[0])
-		president.append(row[2])
+    reader = csv.reader(csvfile)
+    for row in reader:
+        president.append(row[0])
+        president.append(row[2])
+
+print president
