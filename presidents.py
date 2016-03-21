7174e49eb37aed001bf6c8af9efac76412e84488
Clarisa Carruthers <clarisita.92@gmail.com>
2016-03-20 23:18:15 -0400

    for row in presidents

--- presidents.py
+++ presidents.py
@@ -12,4 +12,6 @@ print presidents
 
 with open('output.txt', 'w') as csvfile:
     writer = csv.writer(csvfile)
+    for row in presidents:
+        writer.writerow(row)
     writer.writerow(['president', 'party'])
